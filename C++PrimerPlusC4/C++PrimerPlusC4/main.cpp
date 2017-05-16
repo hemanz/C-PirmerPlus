01ce90374ec04ae20d20a0a5fe79f5b0dd7e2cfd
zhanghenan <zhanghenan@sensetime.com>
2017-05-16 15:16:08 +0800

    init Chapter 4

---
+++ C++PrimerPlusC4/C++PrimerPlusC4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++PrimerPlusC4
+//
+//  Created by zhanghenan on 2017/5/16.
+//  Copyright © 2017年 HN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
