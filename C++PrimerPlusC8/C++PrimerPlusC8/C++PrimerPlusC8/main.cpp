b4f1fd254863ca377683f4c1674f09dd88ff7c1b
zhanghenan <zhanghenan@sensetime.com>
2017-05-25 10:43:12 +0800

    Template function

---
+++ C++PrimerPlusC8/C++PrimerPlusC8/C++PrimerPlusC8/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  C++PrimerPlusC8
+//
+//  Created by zhanghenan on 2017/5/25.
+//  Copyright © 2017年 HN. All rights reserved.
+//
+
+#include <iostream>
+
+template <typename T>
+void Swap(T &a,T &b);
+
+int main(int argc, const char * argv[]) {
+    int i = 10;
+    int j = 20;
+    std::cout << "i = " << i << " j = " << j << std::endl;
+    std::cout << "Swap---" << std::endl;
+    Swap(i, j);
+    std::cout << "i = " << i << " j = " << j << std::endl;
+    
+    double x = 20.8;
+    double y = 88.9;
+    std::cout << "x = " << x << " y = " << y << std::endl;
+    std::cout << "Swap---" << std::endl;
+    Swap(x, y);
+    std::cout << "x = " << x << " y = " << y << std::endl;
+
+    return 0;
+}
+
+template <typename T>
+void Swap(T &a,T &b){
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
