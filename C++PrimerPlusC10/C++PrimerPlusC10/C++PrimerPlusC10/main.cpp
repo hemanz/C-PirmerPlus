3f156192a4b2d8aecfc0b360b63f950a9bf65b18
zhanghenan <zhanghenan@sensetime.com>
2017-05-30 23:25:11 +0800

    Add some test on stock10

--- C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/main.cpp
+++ C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/main.cpp
@@ -7,8 +7,12 @@
 //
 
 #include <iostream>
+#include "stock10.h"
 
 int main(int argc, const char * argv[]) {
-
+    using std::cout;
+    cout << "Using constructor to create new objects\n";
+    Stock stock1("NanoSmart", 12, 20.0);
+    stock1.show();
     return 0;
 }
