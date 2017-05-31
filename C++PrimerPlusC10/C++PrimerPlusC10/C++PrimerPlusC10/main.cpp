85470ee8526d56f7df73a2f43600c47784913e96
zhanghenan <zhanghenan@sensetime.com>
2017-05-31 14:29:40 +0800

    Test destructor function

--- C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/main.cpp
+++ C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/main.cpp
@@ -10,9 +10,26 @@
 #include "stock10.h"
 
 int main(int argc, const char * argv[]) {
-    using std::cout;
-    cout << "Using constructor to create new objects\n";
-    Stock stock1("NanoSmart", 12, 20.0);
-    stock1.show();
+    {
+        using std::cout;
+        cout << "Using constructor to create new objects \n";
+        Stock stock1("NanoSmart", 12, 20.0);
+        stock1.show();
+        
+        Stock stock2 = Stock("Buffo Objects", 2, 2.0);
+        stock2.show();
+        
+        cout << "Assingning stock1 to stcok2: \n";
+        stock2 = stock1;
+        cout << "Listing stock1 to stock2: \n";
+        stock1.show();
+        stock2.show();
+        
+        cout << "Using a constructor to reset an object \n";
+        stock1 = Stock("Nifty Foods", 10, 50.0);
+        cout << "Revised stock1:\n";
+        stock1.show();
+        cout << "Done" << std::endl;
+    }
     return 0;
 }
--- C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/stock10.cpp
+++ C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/stock10.cpp
@@ -80,11 +80,11 @@ void Stock::show()
     ios_base::fmtflags orig = cout.setf(ios_base::fixed, ios_base::floatfield);
     std::streamsize prec = cout.precision(3);
     
-    cout << "Company: " << company << " Shares : " << shares << "/n";
+    cout << "Company: " << company << " Shares : " << shares << " \n";
     cout << "Share Price: $" << share_val;
     
     cout.precision(2);
-    cout << " Total Worth: $" << total_val << "\n";
+    cout << " Total Worth: $" << total_val << " \n";
     
     cout.setf(orig,ios_base::floatfield);
     cout.precision(prec);
