66141e62a23596587596e768c57048be205b6245
zhanghenan <zhanghenan@sensetime.com>
2017-06-02 16:39:43 +0800

    Use const top function

--- C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/main.cpp
+++ C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/main.cpp
@@ -9,27 +9,28 @@
 #include <iostream>
 #include "stock10.h"
 
+const int STKS = 4;
 int main(int argc, const char * argv[]) {
     {
-        using std::cout;
-        cout << "Using constructor to create new objects \n";
-        Stock stock1("NanoSmart", 12, 20.0);
-        stock1.show();
+        Stock stocks[STKS] = {
+            Stock("NanoSmart", 12, 20),
+            Stock("Boffo Objects", 200, 2.0),
+            Stock("Monolithic", 130, 3.25),
+            Stock("Fleep Enterprise", 60, 6.5)
+        };
         
-        Stock stock2 = Stock("Buffo Objects", 2, 2.0);
-        stock2.show();
+        std::cout << "Stock holding: \n";
+        int st;
+        for (st = 0; st < STKS; st++)
+            stocks[st].show();
         
-        cout << "Assingning stock1 to stcok2: \n";
-        stock2 = stock1;
-        cout << "Listing stock1 to stock2: \n";
-        stock1.show();
-        stock2.show();
+        const Stock *top = &stocks[0];
+        for (st = 1; st < STKS; st++)
+            top = &top->topval(stocks[st]);
         
-        cout << "Using a constructor to reset an object \n";
-        stock1 = Stock("Nifty Foods", 10, 50.0);
-        cout << "Revised stock1:\n";
-        stock1.show();
-        cout << "Done" << std::endl;
-    }
+        std::cout << "Now the Most Valuble Holding is :\n";
+        top->show();
+    
     return 0;
+    }
 }
