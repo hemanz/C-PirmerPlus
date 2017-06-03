096fa0fcd068141b6d04beaaf132afce1b1dba37
zhanghenan <zhanghenan@sensetime.com>
2017-06-03 23:49:03 +0800

    Add Stack.h

---
+++ C++PrimerPlusC10/C++PrimerPlusC10/C++PrimerPlusC10/stack.h
@@ -0,0 +1,29 @@
+//
+//  stack.h
+//  C++PrimerPlusC10
+//
+//  Created by zhanghenan on 2017/6/3.
+//  Copyright © 2017年 HN. All rights reserved.
+//
+
+#ifndef stack_h
+#define stack_h
+
+typedef unsigned long Item;
+
+class Stack
+{
+private:
+    enum {MAX = 10};
+    Item item[MAX];
+    int top;
+
+public:
+    Stack();
+    bool isempty() const;
+    bool isfill() const;
+    bool push(const Item &item);
+    bool pop(Item &item);
+};
+
+#endif /* stack_h */
