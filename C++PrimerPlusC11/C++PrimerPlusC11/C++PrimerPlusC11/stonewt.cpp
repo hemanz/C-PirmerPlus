a21b94d2908f890e842565a47ca1f9e82562dea9
zhanghenan <zhanghenan@sensetime.com>
2017-06-05 17:20:43 +0800

    Add stonewt cpp

--- C++PrimerPlusC11/C++PrimerPlusC11/C++PrimerPlusC11/stonewt.cpp
+++ C++PrimerPlusC11/C++PrimerPlusC11/C++PrimerPlusC11/stonewt.cpp
@@ -5,5 +5,41 @@
 //  Created by zhanghenan on 2017/6/5.
 //  Copyright © 2017年 HN. All rights reserved.
 //
-
+#include <iostream>
 #include "stonewt.hpp"
+
+using std::cout;
+
+Stonewt::Stonewt(double lbs)
+{
+    stone = int (lbs);
+    pds_left = int (lbs);
+    pounds = lbs;
+}
+
+Stonewt::Stonewt(int stn, double lbs)
+{
+    stone = stn;
+    pds_left = lbs;
+    pounds = stn * Lbs_per_stn;
+}
+
+Stonewt::Stonewt()
+{
+    stone = pounds = pds_left = 0;
+}
+
+Stonewt::~Stonewt()
+{
+    
+}
+
+void Stonewt::show_stn() const
+{
+    cout << stone << " stone" <<  pds_left << "pounds\n";
+}
+
+void Stonewt::show_lbs() const
+{
+    cout << pounds <<  "pounds\n";
+}
