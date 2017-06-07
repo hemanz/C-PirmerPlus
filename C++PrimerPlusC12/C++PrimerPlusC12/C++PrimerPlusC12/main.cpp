826b00ccc79cf2c53bbf29c50edce8fc6f6c42a1
zhanghenan <zhanghenan@sensetime.com>
2017-06-07 19:45:35 +0800

    test BadString

--- C++PrimerPlusC12/C++PrimerPlusC12/C++PrimerPlusC12/main.cpp
+++ C++PrimerPlusC12/C++PrimerPlusC12/C++PrimerPlusC12/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+#include "stringbad.hpp"
+using std::cout;
+
+void callme1(StringBad &);
+void callme2(StringBad);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    using std::endl;
+    {
+        cout << "Starting an inner block. \n";
+        StringBad headline1("Celery Stalks at Midnight");
+        StringBad headline2("Lettuce Prey");
+        StringBad sports("Spinach Leaves Bowl for Dollars");
+        cout << "headline1:" << headline1 << endl;
+        cout << "headline2:" << headline2 << endl;
+        cout << "sports:" << sports << endl;
+        callme1(headline1);
+        cout << "headline1:" << headline1 << endl;
+        callme2(headline2);
+        cout << "headline2:" << headline2 << endl;
+        cout << "Initialize one object to another:\n";
+        StringBad sailor = sports;
+        cout << "sailor:" << sailor << endl;
+        cout << "Assign one object to another:\n";
+        StringBad knot;
+        knot = headline1;
+        cout << "knot:" << knot << endl;
+        cout << "Exiting the Block";
+    }
     return 0;
+}
+
+void callme1(StringBad &rsb)
+{
+    cout << "String passed by reference:\n";
+    cout << "   \"" << rsb << "\"\n";
+}
+
+void callme2(StringBad sb)
+{
+    cout << "String passed by value:\n";
+    cout << "   \"" << sb << "\"\n";
 }
