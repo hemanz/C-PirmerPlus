5fc3d552344fa618a8b29a36df2a78064854270b
zhanghenan <zhanghenan@sensetime.com>
2017-06-23 21:42:12 +0800

    update string

---
+++ C++PrimerPlusC12/C++PrimerPlusC12/C++PrimerPlusC12/string1.cpp
@@ -0,0 +1,16 @@
+//
+//  string1.cpp
+//  C++PrimerPlusC12
+//
+//  Created by zhanghenan on 2017/6/22.
+//  Copyright © 2017年 HN. All rights reserved.
+//
+
+#include "string1.hpp"
+using std::cin;
+using std::cout;
+
+int String::HowMany()
+{
+    return num_string;
+}
